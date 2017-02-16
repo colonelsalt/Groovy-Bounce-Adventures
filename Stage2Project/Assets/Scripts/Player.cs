17749646454471ef17c0b9081d6fbbde2bfadef3
Sondre Agledahl <sondre.milch@gmail.com>
2017-02-16 02:22:25 +0000

    Improved on basic controls; should remove sliding-off-edge-glitch with use of Mathf.Clamp and Arena.Width/Height for next commit

--- Stage2Project/Assets/Scripts/Player.cs
+++ Stage2Project/Assets/Scripts/Player.cs
@@ -11,48 +11,58 @@ public class Player : MonoBehaviour
     private Rigidbody mBody;
 	private Vector3 directionForce;
 	private Vector3 directionDistance;
+	private bool touchingHorizontal, touchingVertical;
 
     void Awake()
     {
+    	touchingHorizontal = touchingVertical = false;
         mBody = GetComponent<Rigidbody>();
 		directionForce = -Vector3.forward;
     }
 
-    void OnCollisionStay(Collision col)
+    void OnCollisionEnter(Collision col)
     {
     	Debug.Log("Touching " + col.gameObject.tag);
 
     	if (col.gameObject.tag == "Horizontal wall")
     	{
-			directionDistance = Vector3.zero;
-			if (Input.GetKey(KeyCode.A))
-	        {
-	            directionDistance += -Vector3.right * Speed * Time.deltaTime;
-	        }
-	        else if (Input.GetKey(KeyCode.D))
-	        {
-	            directionDistance += Vector3.right * Speed * Time.deltaTime;
-	        }
-    	}
+    		touchingHorizontal = true;
+    		touchingVertical = false;
+		}
     	else if (col.gameObject.tag == "Vertical wall")
     	{
-			directionDistance = Vector3.zero;
-			if (Input.GetKey(KeyCode.W))
-	        {
-	            directionDistance += Vector3.forward * Speed * Time.deltaTime;
-	        }
-	        else if (Input.GetKey(KeyCode.S))
-	        {
-	            directionDistance += -Vector3.forward * Speed * Time.deltaTime;
-	        }
-    	}
-		transform.position += directionDistance;
-		
+    		touchingVertical = true;
+    		touchingHorizontal = false;
+		}
+    }
+    void OnCollisionExit(Collision col)
+    {
+    	Debug.Log("Collision ended!");
+    	if (col.gameObject.tag == "Horizontal wall") touchingHorizontal = false;
+    	else if (col.gameObject.tag == "Vertical wall") touchingVertical = false;
     }
 
     void Update()
     {
-    	
+		directionDistance = Vector3.zero;
+		if (Input.GetKey(KeyCode.A) && touchingHorizontal)
+        {
+            directionDistance += -Vector3.right * Speed * Time.deltaTime;
+        }
+		else if (Input.GetKey(KeyCode.D) && touchingHorizontal)
+        {
+            directionDistance += Vector3.right * Speed * Time.deltaTime;
+        }
+		else if (Input.GetKey(KeyCode.W) && touchingVertical)
+        {
+            directionDistance += Vector3.forward * Speed * Time.deltaTime;
+        }
+		else if (Input.GetKey(KeyCode.S) && touchingVertical)
+        {
+            directionDistance += -Vector3.forward * Speed * Time.deltaTime;
+        }
+
+		transform.position += directionDistance;
 		mBody.AddForce(directionForce * Speed * Time.deltaTime);
     }
 }
