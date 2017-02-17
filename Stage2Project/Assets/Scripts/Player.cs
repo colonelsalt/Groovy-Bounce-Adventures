2b1b85555c28a05a13407fc0b3627a3515e7ba4d
Sondre Agledahl <sondre.milch@gmail.com>
2017-02-17 00:39:31 +0000

    Implemented primitive bouncing mechanism; some glitches with freezing player position when hitting opposite wall still remain.

--- Stage2Project/Assets/Scripts/Player.cs
+++ Stage2Project/Assets/Scripts/Player.cs
@@ -7,27 +7,34 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     public float Speed;
+    public float bounceFactor;
 
     private Rigidbody mBody;
 	private Vector3 directionForce;
-	private Vector3 directionDistance;
-	private bool touchingHorizontal, touchingVertical;
+	private bool touchingHorizontal, touchingVertical, movedLinear, bounced;
 
     void Awake()
     {
-    	touchingHorizontal = touchingVertical = false;
+    	touchingHorizontal = touchingVertical = movedLinear = bounced = false;
         mBody = GetComponent<Rigidbody>();
-		directionForce = -Vector3.forward;
     }
 
-    void OnCollisionEnter(Collision col)
+    void OnTriggerEnter(Collider col)
     {
     	Debug.Log("Touching " + col.gameObject.tag);
 
-    	if (col.gameObject.tag == "Horizontal wall") touchingHorizontal = true;
-    	else if (col.gameObject.tag == "Vertical wall") touchingVertical = true;
+    	if (col.gameObject.tag == "Horizontal wall")
+    	{
+    		touchingHorizontal = true;
+    		FreezePosition();
+		}
+    	else if (col.gameObject.tag == "Vertical wall")
+    	{
+    		touchingVertical = true;
+    		FreezePosition();
+		}
     }
-    void OnCollisionExit(Collision col)
+    void OnTriggerExit(Collider col)
     {
     	Debug.Log("Collision ended!");
     	if (col.gameObject.tag == "Horizontal wall") touchingHorizontal = false;
@@ -39,24 +46,61 @@ public class Player : MonoBehaviour
 		if (Input.GetKey(KeyCode.A) && touchingHorizontal)
         {
             transform.position += -Vector3.right * Speed * Time.deltaTime;
+            movedLinear = true;
+            bounced = false;
         }
 		else if (Input.GetKey(KeyCode.D) && touchingHorizontal)
         {
             transform.position += Vector3.right * Speed * Time.deltaTime;
+            movedLinear = true;
+            bounced = false;
         }
 		else if (Input.GetKey(KeyCode.W) && touchingVertical)
         {
             transform.position += Vector3.forward * Speed * Time.deltaTime;
+            movedLinear = true;
+            bounced = false;
         }
 		else if (Input.GetKey(KeyCode.S) && touchingVertical)
         {
             transform.position += -Vector3.forward * Speed * Time.deltaTime;
+            movedLinear = true;
+            bounced = false;
         }
-        // contain player within bounds of outer walls
-		float newX = Mathf.Clamp(transform.position.x, (-Arena.Width / 2) + VerticalWall.Width, (Arena.Width / 2) - VerticalWall.Width);
-		float newZ = Mathf.Clamp(transform.position.z, (-Arena.Height / 2) + HorizontalWall.Height, (Arena.Height / 2) - HorizontalWall.Height);
 
-		transform.position = new Vector3(newX, transform.position.y, newZ);
-		mBody.AddForce(directionForce * Speed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.Space) && (touchingVertical || touchingHorizontal))
+        {
+        	mBody.constraints = RigidbodyConstraints.None;
+			if (touchingHorizontal && !touchingVertical)
+        	{ // assign bouncing force depending on whether player is at top or bottom of the screen
+        		Debug.Log("Bounced vertical");
+        		directionForce = (transform.position.z > 0) ? bounceFactor * Vector3.back : bounceFactor * Vector3.forward;
+			} 
+			else if (touchingVertical && !touchingHorizontal)
+        	{ // assign bouncing force depending on whether player is at the left or at the right side of the screen
+        		Debug.Log("Bounced horizontal");
+        		directionForce = (transform.position.x > 0) ? bounceFactor * Vector3.left : bounceFactor * Vector3.right; 
+        	}
+        	bounced = true;
+        	movedLinear = false;
+        	touchingHorizontal = touchingVertical = false;
+        	Debug.Log("Bounced by " + directionForce);
+        }
+
+
+        if (movedLinear)
+        {
+			// contain player within bounds of outer walls
+			float newX = Mathf.Clamp(transform.position.x, (-Arena.Width / 2) + VerticalWall.Width, (Arena.Width / 2) - VerticalWall.Width);
+			float newZ = Mathf.Clamp(transform.position.z, (-Arena.Height / 2) + HorizontalWall.Height, (Arena.Height / 2) - HorizontalWall.Height);
+			transform.position = new Vector3(newX, transform.position.y, newZ);
+        }
+        else if (bounced) mBody.AddForce(directionForce * Speed * Time.deltaTime);
     }
+
+	private void FreezePosition()
+	{
+		mBody.constraints = RigidbodyConstraints.FreezeAll;
+	}
 }
+
